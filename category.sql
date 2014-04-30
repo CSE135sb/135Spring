8865d67980b7cc9819f39490bc8f9d3c879ef975
davidweinfls <davidweinfls@163.com>
2014-04-29 17:55:54 -0700

    sql files

--- category.sql
+++ category.sql
@@ -1,8 +1,8 @@
-CREATE TABLE category (
+CREATE TABLE categories (
 	id		SERIAL PRIMARY KEY,
 	c_name		TEXT,
 	description	TEXT
 );
 
-INSERT INTO category (c_name, description) values ('birth control', 'prevent your girlfriend from pregnancy');
+INSERT INTO categories (c_name, description) values ('birth control', 'prevent your girlfriend from pregnancy');
 
--- product.sql
+++ product.sql
@@ -1,10 +1,11 @@
-CREATE TABLE product (
-    id          SERIAL PRIMARY KEY,
+CREATE TABLE products (
+    id          	SERIAL PRIMARY KEY,
     p_name 		TEXT,
-    sku         INTEGER, 
+    sku         	INTEGER, 
+    category_id		INTEGER REFERENCES categories (ID) NOT NULL,
     price 		INTEGER
 );
 
-INSERT INTO product (p_name, sku, price) values ('derux', 2001, 6);
-INSERT INTO product (p_name, sku, price) values ('trojan', 1203, 3);
+INSERT INTO products (p_name, sku, category_id, price) values ('derux', 2001, 1, 6);
+INSERT INTO products (p_name, sku, category_id, price) values ('trojan', 1203, 1, 3);
 
