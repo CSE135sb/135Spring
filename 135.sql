4df0691b5f39f99d75a99f7bbc78633c120f6dc1
davidweinfls <davidweinfls@163.com>
2014-05-01 14:55:51 -0700

    5/1 new db file. with initial inputs

--- 135.sql
+++ 135.sql
@@ -43,7 +43,20 @@ CREATE TABLE purchased_items(
   owner   INTEGER REFERENCES customers (ID) NOT NULL
 );
 
--- INSERT INTO categories (c_name, description) values ('birth control', 'prevent your girlfriend from pregnancy');
--- INSERT INTO products (p_name, sku, category_id, price) values ('derux', 2001, 1, 6);
--- INSERT INTO products (p_name, sku, category_id, price) values ('trojan', 1203, 1, 3);
+ INSERT INTO categories (c_name, description) values ('food', 'eat..');
+ INSERT INTO categories (c_name, description) values ('drink', 'drink..');
+ INSERT INTO categories (c_name, description) values ('birth control', 'prevent your girlfriend from pregnancy');
+
+ INSERT INTO products (p_name, sku, category_id, price) values ('durex', 2001, 3, 6);
+ INSERT INTO products (p_name, sku, category_id, price) values ('durex1', 2002, 3, 7);
+ INSERT INTO products (p_name, sku, category_id, price) values ('durex2', 2003, 3, 8);
+ INSERT INTO products (p_name, sku, category_id, price) values ('okamato', 2012, 3, 10);
+ INSERT INTO products (p_name, sku, category_id, price) values ('jissbon', 2341, 3, 2);
+ INSERT INTO products (p_name, sku, category_id, price) values ('trojan', 1203, 1, 3);
+
+
+ INSERT INTO products (p_name, sku, category_id, price) values ('banana', 0001, 1, 2);
+ INSERT INTO products (p_name, sku, category_id, price) values ('banana chips', 0002, 1, 5);
+ INSERT INTO products (p_name, sku, category_id, price) values ('banana milk', 0010, 2, 10);
+ INSERT INTO products (p_name, sku, category_id, price) values ('apple juice', 1222, 2, 30);
 
